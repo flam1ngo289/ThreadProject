0f5eafbb4c7a48ba6638b5cad76924f365db44fd
flam1ngo <kris.s.03@mail.ru>
2024-09-03 11:13:23 +0300

    Initial commit

---
+++ PoolDiploma/PoolDiploma.cpp
@@ -0,0 +1,22 @@
+﻿#include "SafeQueue.h"
+#include "ThreadPool.h"
+
+
+void doWork1() {
+    std::cout << "Функция: " << __FUNCTION__ << " сделала свою работу!" << std::endl;
+}
+
+void doWork2() {
+    std::cout << "Функция: " << __FUNCTION__ << " сделала свою работу!" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "rus");
+    const int cores = std::thread::hardware_concurrency();
+    std::vector<std::function<void()>> vectorOfFunctions{ doWork1, doWork2 };
+    int countSubmits = 8;
+    ThreadPool thread_pool{ cores };
+    std::thread thread1(&ThreadPool::submit, &thread_pool, vectorOfFunctions, countSubmits);
+    thread1.join();
+    return 0;
+}
